2d10f55a2fcdad0d8aafa2b0de5a8e6c15182ae0
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-23 20:47:31 -0300

    eu sou groot

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+bool groot = true;
+string resposta;
+ Console.WriteLine("--- Groot Chatbot ---");
+
+while (groot)
+{
+    Console.Write($"Pergunta: ");
+    resposta = Console.ReadLine()!;
+    Console.WriteLine("Resposta: Eu sou Groot.");
+
+    if (resposta == "Tchau")
+    {
+
+        break;
+}
+}
+
